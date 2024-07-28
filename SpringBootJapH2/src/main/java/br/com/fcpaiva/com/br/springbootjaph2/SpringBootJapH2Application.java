84160460569e26866179070b58473078a898df38
Fernando Campos de Paiva <fernandocampospaiva@hotmail.com>
2024-07-28 12:17:51 -0300

    First commit

---
+++ SpringBootJapH2/src/main/java/br/com/fcpaiva/com/br/springbootjaph2/SpringBootJapH2Application.java
@@ -0,0 +1,13 @@
+package br.com.fcpaiva.com.br.springbootjaph2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJapH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJapH2Application.class, args);
+    }
+
+}
---
+++ SpringBootJapH2/src/test/java/br/com/fcpaiva/com/br/springbootjaph2/SpringBootJapH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fcpaiva.com.br.springbootjaph2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJapH2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
